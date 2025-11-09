690d9d872ca74a5f78b07cdf7e07b0b0452f64fe
sudohelal <helaluddin40@outlook.com>
2025-11-09 12:39:34 +0600

    Login page design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center items-center min-h-screen'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                    <h1 className="text-3xl font-bold text-center">Login Now</h1>
+                    <form>
+                        <fieldset className="fieldset">
+                            {/* name */}
+                            <label className="label">Name</label>
+                            <input type="text" className="input" placeholder="Name" />
+                            {/* image */}
+                            <label className="label">ImageURL</label>
+                            <input type="text" className="input" placeholder="ImageUrl" />
+                            {/* email */}
+                            <label className="label">Email</label>
+                            <input type="email" className="input" placeholder="Email" />
+                            {/* password */}
+                            <label className="label">Password</label>
+                            <input type="password" className="input" placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4">LogIn Now</button>
+                        </fieldset>
+                    </form>
+                    <p>Don't  have account? please <Link className='text-[#7b6557] underline' to="/auth/register">Register</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
